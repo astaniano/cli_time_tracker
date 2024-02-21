2157a11902ff6c55e2fcd5651898db407e85d81c
astaniano <50764211+astaniano@users.noreply.github.com>
2024-02-21 21:12:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ if len(cli_args) < 2:
 
 command = cli_args[1]
 
-if command == 'st':
+if command == 'str':
     handle_start(cli_args)
-elif command == 'sp':
+elif command == 'stp':
     handle_stop()
 elif command == 'ls':
     handle_report(cli_args)
